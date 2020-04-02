efc67755d7571344f64869bf644b5e7ef6d7aa79
thanhson0514 <thanhson0514@gmail.com>
2020-04-02 15:48:25 +0700

    nothing

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,13 @@
 const axios = require("axios");
 
+const sendTextMessage = require("../cron-scheduler/send-text-message");
+
 exports.getUser = async (req, res) => {
   try {
+    sendTextMessage(
+      process.env.FACEBOOK_ID_USER,
+      "Có 1 bạn đã đăng nhập vào web của bạn. Hãy kiểm tra nào!!"
+    );
     const response = await axios.get(
       "http://sv20.tlu.edu.vn:8092/education/api/student/531",
       {
--- cron-scheduler/process-scheduler.js
+++ cron-scheduler/process-scheduler.js
@@ -2,18 +2,20 @@ const Scheduler = require("../models/scheduler");
 const schedule = require("node-schedule");
 const sendTextMessage = require("./send-text-message");
 
+const { FACEBOOK_ID_USER } = process.env;
+
 exports.notication = async (hour = 22, minutes = 30) => {
   try {
     const scheduler = await Scheduler.find();
-    const id = scheduler[0].idUser;
+
     scheduler.forEach(el => {
       //   console.log(el)
       schedule.scheduleJob(
         `0 ${el.startHour.split("h")[0]} ${el.startHour.split("h")[1] -
-          2} * * ${el.date}`,
+          2} * * ${el.date - 1}`,
         () => {
           return sendTextMessage(
-            el.idUser,
+            Number(el.idUser),
             `Hôm nay có môn ${el.displayName} vào lúc ${
               el.startHour.split("h")[0]
             }:${el.startHour.split("h")[1]} nhớ đi học nha. Yêu <3!`
@@ -24,7 +26,7 @@ exports.notication = async (hour = 22, minutes = 30) => {
 
     schedule.scheduleJob(`00 ${minutes + 2} ${hour} * * *`, () => {
       sendTextMessage(
-        id,
+        FACEBOOK_ID_USER,
         "Good Night, Buổi tối vui vẻ code nhiều bug :). Yêu Sơn <3 !"
       );
     });
